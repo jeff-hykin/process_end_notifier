f2afa04c74f5ed2544302af45eba3146082b7ad5
Jeff Hykin <jeff.hykin@gmail.com>
2023-08-14 16:50:21 -0500

    -

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const envVarName = `DENO_TELEGRAM_BOT_TOKEN`
 // 
 // these are the only OS-specific functions
 // 
-const listProcessesWithName = async (name)=>(await run`ps -axww -o pid,command ${Stdout(returnAsString)}`).split("\n").filter(each=>each.match(name)).map(each=>`    ${each.split(/[ \t]+/).map((each,index)=>index==0?green(each):each).join(" ")}`).join("\n")
+const listProcessesWithName = async (name)=>(await run`ps -axww -o pid,command ${Stdout(returnAsString)}`).split("\n").filter(eachLine=>eachLine.match(name)).map(eachLine=>`    ${eachLine.split(/(?<!^)[ \t\s\n\r]+/).map((each,index)=>index==0?green(each):each).join(" ")}`).join("\n")
 const processIsStillRunning = async (pid)=>(await run`ps -p ${pid} ${Stdout(returnAsString)}`).match(pid)
 const maybeProcessDuration = async (pid)=>{
     const status = (await run`ps -p ${pid} ${Stdout(returnAsString)}`)
